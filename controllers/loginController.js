d23978bd6915e5f4a1651278b56659b39f4d9aeb
Bryan <bryanmurasira@gmail.co>
2023-03-13 19:11:56 +0200

    feat(loginController): modified the controller
    - added the tryCatch components

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,37 +4,42 @@ const jwt = require('jsonwebtoken');
 
 
 const authorize = async( req, res) => {
-    const { eml, pwd} = req.body;
-    if( !eml || !pwd) return res.status(400).json({"message": "Problem with request body."});
+    try {
+        const { eml, pwd} = req.body;
+        if( !eml || !pwd) return res.status(400).json({"message": "Problem with request body."});
 
-    const authUser = await User.findOne({ email: eml});
-    if( !authUser) return res.status(404).json({"message": "This user's email is NOT registered."});
+        const authUser = await User.findOne({ email: eml});
+        if( !authUser) return res.status(404).json({"message": "This user's email is NOT registered."});
 
-    const correctPassword = await bcrypt.compare( pwd, authUser.password);
-    if(correctPassword){
-        const accessToken = jwt.sign(
-            { name: authUser.firstName, email: authUser.email, roles: Object.values(authUser.roles)},
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '50s', issuer: "portfolio-server" }
-        );
-        const refreshToken = jwt.sign(
-            { userId: authUser.id },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: '7d', issuer: "portfolio-server" }
-        );
-        authUser.refreshToken = refreshToken;
-        await authUser.save();
-        
-        
-        res.cookie( "refreshToken", refreshToken ,{
-            httpOnly: true,
-            sameSite: 'none',
-            maxAge: 7 * 24 * 60 * 60 * 1000} //'add secure: true,' in the options when you stop using thunder client
-        );
-        
-        res.json({accessToken});
-    } else {
-        res.status(401).json({"message": "Incorrect Password."});
+        const correctPassword = await bcrypt.compare( pwd, authUser.password);
+        if(correctPassword){
+            const accessToken = jwt.sign(
+                { name: authUser.firstName, email: authUser.email, roles: Object.values(authUser.roles)},
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '50s', issuer: "portfolio-server" }
+            );
+            const refreshToken = jwt.sign(
+                { userId: authUser.id },
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: '7d', issuer: "portfolio-server" }
+            );
+            authUser.refreshToken = refreshToken;
+            await authUser.save();
+            
+            
+            res.cookie( "refreshToken", refreshToken ,{
+                httpOnly: true,
+                sameSite: 'none',
+                maxAge: 7 * 24 * 60 * 60 * 1000} //'add secure: true,' in the options when you stop using thunder client
+            );
+            
+            res.json({accessToken});
+        } else {
+            res.status(401).json({"message": "Incorrect Password."});
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ "message": "Internal Server Error."});
     }
 }
 
