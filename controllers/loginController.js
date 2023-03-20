606b8513ad7460eb5696882ba0854443f8a7108d
M936-B <84919967+M936-B@users.noreply.github.com>
2023-03-21 00:27:59 +0200

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -31,7 +31,7 @@ const authorize = async( req, res) => {
                 httpOnly: true,
                 sameSite: 'none',
                 secure: true,
-                maxAge: 7 * 24 * 60 * 60 * 1000,
+                expiresIn: '7d',
                 path: '/'
             } //'add secure: true,' in the options when you stop using thunder client
             );
