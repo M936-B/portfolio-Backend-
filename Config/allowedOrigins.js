9a963f627039122886f0f2860f3c8a8cf89a84a9
M936-B <84919967+M936-B@users.noreply.github.com>
2023-03-20 23:07:14 +0200

    Update allowedOrigins.js

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,9 +1,9 @@
 
 
 const allowedOrigins = [
-    'https://cute-pear-sheep-slip.cyclic.app/',
-    'https://bryanmurasira.netlify.app/',
-    'http://127.0.0.1:5500/'
+    'https://cute-pear-sheep-slip.cyclic.app',
+    'https://bryanmurasira.netlify.app',
+    'http://127.0.0.1:5500'
 ]
 
 
